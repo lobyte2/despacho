65b6fa16a38ee15756a52a1406e0eedbb48b4022
lobyte2 <l.uribe@duocuc.cl>
2026-05-10 00:23:22 -0400

    ip

--- src/componentes/CrudAdmin/TableCompras.jsx
+++ src/componentes/CrudAdmin/TableCompras.jsx
@@ -7,7 +7,7 @@ export const TableCompras = () => {
   const [ventas, setVentas] = useState([]);
 
   const compras = async () => {
-    await axios.get("http://192.168.30/api/v1/ventas", {
+    await axios.get("http://32.196.41.200:8081/api/v1/ventas", {
       headers:{
         'Content-Type': 'application/json',
         'Accept': 'application/json'
--- src/componentes/CrudAdmin/TableDespachos.jsx
+++ src/componentes/CrudAdmin/TableDespachos.jsx
@@ -8,7 +8,7 @@ export const TableDespachos = () => {
 
   const despacho = async () => {
     await axios
-      .get("http://192.168.3.20/api/v1/despachos", {
+      .get("http://32.196.41.200:8080/api/v1/despachos", {
         headers:{
               'Content-Type': 'application/json',
               'Accept': 'application/json'
